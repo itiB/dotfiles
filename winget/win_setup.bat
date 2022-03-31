7a8a2c7aa52f7a4193ded109d3182912a3f04e45
itiB <is0312vx@ed.ritsumei.ac.jp>
2022-03-31 22:55:00 +0900

    Add: winget scripts

---
+++ winget/win_setup.bat
@@ -0,0 +1,22 @@
+@echo off
+Echo Install Windows Utils
+winget install Microsoft.Powertoys
+winget install 7zip.7zip
+winget install Microsoft.VisualStudioCode
+winget install Docker.DockerDesktop
+winget install python.python
+REM https://zenn.dev/shoji_kai/articles/d26033f252b045
+winget install Git.Git
+winget install GitHub.cli
+winget install Microsoft.GitCredentialManagerCore -s winget
+winget install Notion.Notion
+winget install LINE.LINE
+winget install Discord.Discord
+winget install Mozilla.Firefox
+winget install google.Chrome
+@REM winget install Google.Chrome --location "D:\Program Files\"
+winget install Microsoft.Office
+winget install Valve.Steam
+winget install SlackTechnologies.Slack
+
+wsl --install
