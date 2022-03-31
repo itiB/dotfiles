b93b362d2239da692ca3005bc77d34d83867a0ba
itiB <is0312vx@ed.ritsumei.ac.jp>
2022-04-01 00:14:28 +0900

    Add: Steps for manage apt, symlink

--- init/init.sh
+++ init/init.sh
@@ -15,6 +15,6 @@ sudo -v
 # fi
 
 echo "Run Playbook"
-ansible-playbook ./playbooks/init.yml --ask-become-pass --syntax-check
-# ansible-playbook ./playbooks/init.yml --ask-become-pass --check
+# ansible-playbook ./playbooks/init.yml --ask-become-pass --syntax-check
+ansible-playbook ./playbooks/init.yml --ask-become-pass --check
 # # ansible-playbook ./playbooks/init.yml --ask-become-pass
