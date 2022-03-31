0dcbcf81d1918a4d0ae0be66fcfd45f2f890e11f
itiB <is0312vx@ed.ritsumei.ac.jp>
2022-03-31 22:23:52 +0900

    Add: inital scripts

--- README.md
+++ README.md
@@ -19,11 +19,6 @@ $ git clone https://github.com/itiB/dotfiles.git
 ```
 
 ```
-$ sudo apt install smbclient
-$ sudo mount -t cifs -o guest,uid=1000,gid=1000,nounix,rw,dir_mode=0755,file_mode=0644 //192.168.10.117/share1 ~/nas
-```
-
-```
 $ sudo apt update
 $ sudo apt install software-properties-common
 $ sudo apt-add-repository --yes --update ppa:ansible/ansible
---
+++ init/init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+echo "Use this script in sudo"
+sudo -v
+
+# echo "install Ansible..."
+# sudo apt install --yes software-properties-common
+# sudo apt-add-repository --yes --update ppa:ansible/ansible
+# sudo apt install --yes ansible
+
+# if [ -f "$HOME/.bashrc" ] && [ ! -h "$HOME/.bashrc" ]; then
+#     echo "Backup: .bashrc"
+#     mv "$HOME/.bashrc" "$HOME/.bashrc.bak"
+# fi
+
+echo "Run Playbook"
+ansible-playbook ./playbooks/init.yml --ask-become-pass --syntax-check
+# ansible-playbook ./playbooks/init.yml --ask-become-pass --check
+# # ansible-playbook ./playbooks/init.yml --ask-become-pass
